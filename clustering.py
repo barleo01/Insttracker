418929a1a35621a010a52f5bd98433a7951236d8
piem2503 <maxime.piergiovanni@usherbrooke.ca>
2018-05-15 18:19:37 +0200

    added gitignore for usual python stuff and project data

---
+++ clustering.py
@@ -0,0 +1,12 @@
+"""
+Given a point in an image and a starting point, 
+returns the cluster in which starting point belong.
+
+@author: Maxime Piergiovanni
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+startPointA = (348, 191, 0)
+folderA = './project_data/a/'
